097029b8d71026b96dd7cddb2fbdff80f6395886
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-05-22 20:42:31 +0545

    switch case example

---
+++ SwitchExample.java
@@ -0,0 +1,15 @@
+public class SwitchExample {
+    public static void main(String[] args) {
+        int number=2;
+        switch(number){
+
+            case 1: System.out.println("10");
+                break;
+            case 2: System.out.println("20");
+                break;
+            case 3: System.out.println("30");
+                break;
+            default:System.out.println("Not in 1, 2 or 3");
+        }
+    }
+}
