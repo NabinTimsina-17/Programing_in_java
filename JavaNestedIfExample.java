f2bdebc7dab5a62dd5c7059b63b8f71197102dbc
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-05-22 20:30:45 +0545

    nestedif else statement in java

--- JavaNestedIfExample.java
+++ JavaNestedIfExample.java
@@ -1,12 +1,16 @@
 public class JavaNestedIfExample {
     public static void main(String[] args) {
         //Creating two variables for age and weight
-        int age=20;
-        int weight=51;
+        int age=25;
+        int weight=48;
         //applying condition on age and weight
         if(age>=18){
             if(weight>50){
                 System.out.println("You are eligible to donate blood");
+            } else{
+                System.out.println("You are not eligible to donate blood");
             }
+        } else{
+            System.out.println("Age must be greater than 18");
         }
-    }}
+    }  }
