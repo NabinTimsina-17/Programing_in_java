12352bd9801de9de85f846d5272e73092d5926d8
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-05-25 19:14:54 +0545

    single inheritance

--- ContinueExample.java
+++ ContinueExample.java
@@ -2,7 +2,7 @@ public class ContinueExample {
     public static void main(String[] args) {
         //for loop
         for(int i=1;i<=10;i++){
-            if(i==5){
+            if(i==7){
                 continue;
             }
             System.out.println(i);
