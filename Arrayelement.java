853ee9b6815a237d80d0791e952dd6497bd5fede
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-06-21 22:03:04 +0545

    10 element from user and deleting xth element from user

--- Arrayelement.java
+++ Arrayelement.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
-
-public class Arrayelement {
-    public static void main(String[] args) {
+public class Arrayelement{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[10];
-        System.out.println("Enter 10 elements:");
-        for (int i = 0; i < 10; i++) {
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("Enter the index of the element to be deleted:");
-        int index = sc.nextInt();
-        for (int i = index; i < arr.length - 1; i++) {
-            arr[i] = arr[i + 1];
-        }
-        System.out.println("Array after deleting the element:");
-        for (int i = 0; i < arr.length - 1; i++) {
-            System.out.print(arr[i] + " ");
-        }
+    System.out.println("Enter 10 elements:");
+    for(int i =0; i<10; i++){
+        arr[i]=sc.nextInt();
     }
+    System.out.println("Enter the index of the element to be deleted");
+    int index = sc.nextInt();
+    for (int i = index; i < arr.length-1 ; i++ ){
+        arr[i] = arr[i+1];
+
+    }
+    System.out.println("Array after deleting the element:");
+    for (int i= 0; i<arr.length-1 ; i++){
+      System.out.println(arr[i]+" ");
+    }
+  }
 }
